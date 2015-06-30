c21ff0eed6d12272ca119675b0d082337a81cc54
Ramesh Balasubramanian <ramesh.balasubramanian@gmail.com>
2015-07-01 00:30:53 +0530

    spaces to tabs

--- pangram.py
+++ pangram.py
@@ -5,12 +5,12 @@ import string
 elements = set()
 s = raw_input()
 for ch in s:
-    # print ch
-    if ch != ' ':
-        elements.add(string.lower(ch))
-        if len(elements) == 26:
-            print "pangram"
-            break
+	# print ch
+	if ch != ' ':
+		elements.add(string.lower(ch))
+		if len(elements) == 26:
+			print "pangram"
+			break
 
 if len(elements) != 26:
-    print "not pangram"
+	print "not pangram"
